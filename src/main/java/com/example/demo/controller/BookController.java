ecec8437e5d10b96d8e73032dfc768fd46e0d341
damouu <mouadsehbaoui@gmail.com>
2025-12-17 23:39:02 +0100

    Refactor: BookController (#IAA-3)

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.Book;
 import com.example.demo.service.BookService;
 import lombok.Data;
 import org.springframework.http.MediaType;
@@ -8,19 +7,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
+import java.util.Map;
 
 @Data
 @Validated
 @CrossOrigin
 @RestController
-@RequestMapping("api/public/inventory")
+@RequestMapping("api/public/popularity")
 public class BookController {
 
     private final BookService bookService;
 
-    @GetMapping(path = "/{chapterUUID}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Book> getBookUuid(@PathVariable("chapterUUID") UUID chapterUUID) {
-        return bookService.checkChapterInventory(chapterUUID);
+    @GetMapping(path = "/top-chapters", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> returnBorrowBooks(@RequestParam Map<String, ?> allParams, @RequestParam(defaultValue = "currentweek") String period) {
+        return bookService.topChapters(allParams, period);
     }
 }
