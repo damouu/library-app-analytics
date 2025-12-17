cbfcd62aba79b5f0eb330eeee47da31916644202
damouu <mouadsehbaoui@gmail.com>
2025-12-17 23:41:07 +0100

    Refactor: Borrow (#IAA-3)

---
+++ src/main/java/com/example/demo/model/Borrow.java
@@ -0,0 +1,42 @@
+package com.example.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Borrow {
+
+    @Id
+    @Getter(onMethod = @__(@JsonIgnore))
+    @Column(updatable = false, nullable = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "book_sequence")
+    @SequenceGenerator(name = "book_sequence", allocationSize = 1, sequenceName = "book_sequence")
+    private int id;
+
+    @Column(nullable = false, columnDefinition = "UUID", name = "chapter_uuid")
+    @Getter
+    @Setter
+    private UUID chapterUUID;
+
+    @Column(name = "borrow_date", nullable = false, columnDefinition = "Date")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @Getter(onMethod = @__(@JsonIgnore))
+    @Setter
+    private LocalDate borrowDate;
+
+
+}
