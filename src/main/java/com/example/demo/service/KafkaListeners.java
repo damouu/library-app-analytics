1f714f34aa52b01be07679d1bbf40f0c51e94f68
damouu <mouadsehbaoui@gmail.com>
2025-12-17 23:43:36 +0100

    Refactor: KafkaListeners (#IAA-3)

--- src/main/java/com/example/demo/service/KafkaListeners.java
+++ src/main/java/com/example/demo/service/KafkaListeners.java
@@ -1,7 +1,6 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.BorrowEventPayload;
-import com.example.demo.dto.ReturnEventPayload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -19,11 +18,6 @@ public class KafkaListeners {
 
     @KafkaListener(topics = "library.borrow.v1", groupId = "groupId", containerFactory = "factory")
     void listenerBorrow(@Payload BorrowEventPayload BorrowEventPayload) {
-        bookService.listenerBorrowBooks(BorrowEventPayload, true);
-    }
-
-    @KafkaListener(topics = "library.return.v1", groupId = "groupId", containerFactory = "factory")
-    void listenerReturn(@Payload ReturnEventPayload returnEventPayload) {
-        bookService.listenerReturnBorrowedBooks(returnEventPayload, false);
+        bookService.borrowBooks(BorrowEventPayload);
     }
 }
