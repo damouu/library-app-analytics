ae9fc3c7e1050062456777697b2135763efbfce5
damouu <mouadsehbaoui@gmail.com>
2026-06-25 18:14:26 +0900

    Refactor: ChapterBorrowCountDTO (#IAA-13)

--- src/main/java/com/example/demo/dto/ChapterBorrowCountDTO.java
+++ src/main/java/com/example/demo/dto/ChapterBorrowCountDTO.java
@@ -1,27 +1,14 @@
 package com.example.demo.dto;
 
-import com.fasterxml.jackson.databind.annotation.JsonTypeResolver;
-import lombok.Getter;
-import lombok.Setter;
-
+import java.io.Serializable;
 import java.util.UUID;
 
-@Getter
-@Setter
-public class ChapterBorrowCountDTO {
-    private final UUID chapterUuid;
-    private final Long borrowCount;
-    private final String chapterTitle;
-    private final String chapterSecondTitle;
-    private final int chapterNumber;
-    private final String chapterCoverUrl;
-
-    public ChapterBorrowCountDTO(UUID chapterUuid, Long borrowCount, String chapterTitle, String chapterSecondTitle, int chapterNumber, String chapterCoverUrl) {
-        this.chapterUuid = chapterUuid;
-        this.borrowCount = borrowCount;
-        this.chapterTitle = chapterTitle;
-        this.chapterSecondTitle = chapterSecondTitle;
-        this.chapterNumber = chapterNumber;
-        this.chapterCoverUrl = chapterCoverUrl;
-    }
+public record ChapterBorrowCountDTO(
+        UUID chapterUuid,
+        Long borrowCount,
+        String title,
+        String secondTitle,
+        Integer chapterNumber,
+        String coverArtworkUrl
+) implements Serializable {
 }
